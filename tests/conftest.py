85a442d3f3fdca4c097c5515e1753ea4490bf714
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2025-10-30 22:31:30 -0600

    test: add `setup_forwarder` fixture to initialize forwarder globally in tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,9 +5,20 @@
 #  This software is released under the MIT License.
 #
 #  http://opensource.org/licenses/mit-license.php
+
+# type: ignore
+import logging
+
 import pytest
+
+from mcp_bear.cli import init_forwarder
 
 
 @pytest.fixture(scope="module")
 def anyio_backend() -> str:
     return "asyncio"
+
+
+@pytest.fixture(scope="session", autouse=True)
+def setup_forwarder() -> None:
+    init_forwarder(logging.Logger(__name__))
